887a6081830defe355af24ce613de72d202b6eea
philenius <philippus-perez@web.de>
2017-05-01 18:15:04 +0200

    added App.scala

---
+++ src/main/scala/de/scala/basics/App.scala
@@ -0,0 +1,30 @@
+package de.scala.basics
+
+import de.opitz.bigdata.scala.{Car, CarPark}
+
+object App {
+
+  def main(args: Array[String]): Unit = {
+
+    val carPark = new CarPark(10)
+    val c1 = new Car()
+    val c2 = new Car()
+
+    println(carPark.getFreeParkingSlots)
+    println(carPark.getContainedVehicleCount)
+
+    carPark.driveIn(c1)
+    println(carPark.getFreeParkingSlots)
+    println(carPark.getContainedVehicleCount)
+
+    carPark.driveIn(c2)
+    println(carPark.getFreeParkingSlots)
+    println(carPark.getContainedVehicleCount)
+
+    carPark.driveOut(c1)
+    println(carPark.getFreeParkingSlots)
+    println(carPark.getContainedVehicleCount)
+
+  }
+
+}
